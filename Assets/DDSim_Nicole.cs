73c34665c8ef3738a269bac7c1a67d93568b7a82
Nic0bra <x0obubbleso0x@gmail.com>
2025-01-30 22:49:22 -0600

    added + sign and emojis
    I looked in the book you recommended and found the format for the positive sign but I also happened upon emojis.

--- Assets/DDSim_Nicole.cs
+++ Assets/DDSim_Nicole.cs
@@ -9,6 +9,8 @@ public class DDSim : MonoBehaviour
     [SerializeField][Range(-5, 5)] int Dex;
     [SerializeField] bool finWeapon;
     int Player;
+    public string thumbsdown = char.ConvertFromUtf32(0x1F44E);
+    public string thumbsup = char.ConvertFromUtf32(0x1F44D);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -34,20 +36,20 @@ public class DDSim : MonoBehaviour
         //determine if there was a hit and display results
         if (Player > Comp)
         {
-            Debug.Log(PlayerName + " rolled a " + DIE + ".\n" + PlayerName + "'s strength was " + Player);
-            Debug.Log("The Computer had " + Comp + " armor.");
+            Debug.Log($"{PlayerName} rolled a {DIE}.\n {PlayerName}'s strength was {Player:+0;-0;0;}");
+            Debug.Log($"The Computer had {Comp:+0;-0;0;} armor.");
             Debug.Log(PlayerName + " chopped off the Computer's head.");
             Debug.Log(PlayerName + " smiles as they victoriously bathe in their enemy's blood!");
+            Debug.Log(thumbsup);
 
 
         }
         else
         {
-            Debug.Log(PlayerName + " rolled a " + DIE + ".\n" + PlayerName + "'s strength was " + Player);
-            Debug.Log("The Computer had " + Comp + " armor.");
+            Debug.Log($"{PlayerName} rolled a {DIE}.\n {PlayerName}'s strength was {Player:+0;-0;0;}");
+            Debug.Log($"The Computer had {Comp:+0;-0;0;} armor.");
             Debug.Log(PlayerName + " just wasn't strong enough.\n Try Again if you dare.");
-
-
+            Debug.Log(thumbsdown);
         }
     }
 
